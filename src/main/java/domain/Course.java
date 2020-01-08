ede242dcf865cc11ba15127be7ee729ac2737879
chenruibin <chenruibin@sogou-inc.com>
2020-01-08 11:42:54 +0800

    init

---
+++ src/main/java/domain/Course.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Course {
+    private String name;
+    private List<String> codes;
+    private Map<String, Student> enrolledStudent = new HashMap<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getCodes() {
+        return codes;
+    }
+
+    public void setCodes(List<String> codes) {
+        this.codes = codes;
+    }
+
+    public Map<String, Student> getEnrolledStudent() {
+        return enrolledStudent;
+    }
+
+    public void setEnrolledStudent(Map<String, Student> enrolledStudent) {
+        this.enrolledStudent = enrolledStudent;
+    }
+}
